53e88d7322c1fd69a695b24d5a349f1e2d25db83
Gregor MacLennan <gmaclennan@digital-democracy.org>
2019-10-26 15:24:09 +0100

    Add Android paths

--- path.zsh
+++ path.zsh
@@ -5,3 +5,6 @@ export PATH="node_modules/.bin:$PATH"
 # I've disabled this for now because I only use "ls" which is
 # referenced in my aliases.zsh file directly.
 #export PATH="$(brew --prefix coreutils)/libexec/gnubin:$PATH"
+
+export ANDROID_SDK_ROOT="$(brew --prefix)/share/android-sdk"
+export ANDROID_NDK_HOME="$(brew --prefix)/share/android-ndk"
