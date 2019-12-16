ac3af92cd4989b369849e7c357c0a32830f94b45
Gregor MacLennan <gmaclennan@digital-democracy.org>
2019-12-16 16:29:22 +0000

    Fix Android SDK paths

--- path.zsh
+++ path.zsh
@@ -6,5 +6,5 @@ export PATH="node_modules/.bin:$PATH"
 # referenced in my aliases.zsh file directly.
 #export PATH="$(brew --prefix coreutils)/libexec/gnubin:$PATH"
 
-export ANDROID_SDK_ROOT="$(brew --prefix)/share/android-sdk"
-export ANDROID_NDK_HOME="$(brew --prefix)/share/android-ndk"
+export ANDROID_SDK_ROOT="$HOME/Library/Android/sdk"
+export ANDROID_NDK_HOME="$HOME/Library/Android/sdk/ndk-bundle"
